d72d23b89cf146bbe2161d3a24edd047c225724b
YoYag <la2043432@gmail.com>
2023-05-24 13:02:26 +0900

    메인페이지 작업 완료

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Main from "./routes/Main";
 
 function App() {
   return (
     <div className="m-auto w-1280px flex flex-col items-center ...">
       <Router>
         <Header />
-        Learn React
+        <Routes>
+          <Route path="/" element={<Main />} />
+          Learn React
+        </Routes>
         <Footer />
       </Router>
     </div>
